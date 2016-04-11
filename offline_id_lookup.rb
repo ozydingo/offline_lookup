1489202589201c4a4b10e9126f896b553e73d348
Andrew Schwartz <andrew@3playmedia.com>
2016-04-11 09:29:49 -0400

    use less closures, store info in class attr

--- offline_id_lookup.rb
+++ offline_id_lookup.rb
@@ -1,36 +1,23 @@
+# IMPORTANT: don't use this for models that have a lot of data!!
 #
-# 3Play Media: Cambridge, MA, USA
-# support@3playmedia.com
+# This is basically a in-memory pseudo-index intended to speed up quick, repeated
+# finds of index table values without trips to an external db machine.
+# It's also nicer syntax, e.g:
+# TurnaroundLevel.two_hour_id
+# TurnaroundLevel.quick_lookup("Two Hour")
+# Service.last.turnaround_level.two_hour?
 #
-# Copyright © 2012 3Play Media, Inc.  The following software is the sole and
-# exclusive property of 3Play Media, Inc. and may not to be reproduced,
-# modified, distributed or otherwise used, without the written approval
-# of 3Play Media, Inc.
-#
-# This software is provided "as is" and any express or implied
-# warranties, including but not limited to, an implied warranty of
-# merchantability and fitness for a particular purpose are disclaimed.
-#
-# In no event shall 3Play Media, Inc. be liable for any direct,
-# indirect, incidental, special, exemplary, or consequential damages
-# (including but not limited to, procurement or substitute goods or
-# services, loss of use, data or profits, or business interruption)
-# however caused and on any theory of liability, whether in contract,
-# strict liability, or tort (including negligence or otherwise) arising
-# in any way out of the use of this software, even if advised of the
-# possibility of such damage.
-#
-
 # In any ActiveRecord::Base subclass, use:
-# >>  use_offline_id_lookup column_name
+# >> use_offline_id_lookup column_name
 # to define class methods that can convert between id and the corresponding
 # value in column_name for the class, without going to the database
 # 
 # column_name defaults to :name, but can be any column of the table
+# use the :key keyword arg if you're interested in a key colun other than :id
 #
 # Usage example:
 # class JobType < ActiveRecord::Base
-#   use_offline_id_lookup :name
+#   use_offline_id_lookup
 #   ...
 # end
 #
@@ -58,58 +45,64 @@
 # method will only use the last column for which it was observed.
 
 
-# TODO: provide multiple column names in a single call
+#!!!
+# TODO: requires :methodize
+#!!!
+
+# TODO: provide multiple column names in a single call (e.g. firstname, lastname)
+# TODO: support multiple offline lookups per model
+# TODO: support scope arg in use_offline_lookup (partial index)
 
 module OfflineIDLookup
-  extend ActiveSupport::Concern
+  module ActiveRecord
+    def use_offline_lookup(field = :name, key: :id, identity_methods: false)
+      class_attribute :offline_lookup_values, :offline_lookup_options
+      self.offline_lookup_options = {field: field.to_s, key: key.to_s, identity_methods: identity_methods}.freeze
+      self.offline_lookup_values = self.all.pluck(key, field).to_h.freeze
 
-  module ClassMethods
+      include OfflineIDLookup::Base
+    end
+  end
 
-    #can be used multiple times for different columns
-    def use_offline_id_lookup(field = :name, identity_methods: false)
-      #first find the current values in the database
-      lookup_values = {}
-      self.find_each do |row|
-        lookup_values[row.id] = row[field]
+  module Base
+    extend ActiveSupport::Concern
+
+    # define methods such as :two_hour_id and :id_for_name
+    self.offline_lookup_values.each do |key, value|
+      # e.g., :two_hour_id
+      define_method "#{value.to_s.methodize}_#{self.offline_lookup_options[:key]}" do
+        key
       end
 
-      # define class lookup methods that do not require db access
-      self.singleton_class.instance_eval do
-        # 1. klass.<name>_id
-        lookup_values.each do |id, name|
-          define_method "#{name}_id".methodize do
-            id
-          end
-        end
-
-        # 2. klass.<name>_for_id(id), klass.id_for_<name>(name)
-        define_method "#{field}_for_id" do |id|
-          lookup_values[id]
-        end
-        define_method "id_for_#{field}" do |name|
-          lookup_values.keys.find{|id| lookup_values[id].methodize == name.to_s.methodize}
-        end
-
-        # 3. klass.offline_lookup(<name>)
-        # Actually not really offline but does a search on the indexed primary key, so it's faster
-        define_method "offline_lookup" do |name|
-          name = name.to_s if name.is_a? Symbol
-          self.find(lookup_values.key(name))
+      # e.g., :two_hour?
+      if self.offline_lookup_options[:identity_methods]
+        define_method "#{value.to_s.methodize}?" do
+          self.attributes[self.offline_lookup_options[:key]] == key
         end
       end
-
-      # Define instance methods if requested
-      if identity_methods
-        lookup_values.each do |id, value|
-          define_method("#{value}?".methodize) do
-            self.id == id
-          end
-        end
-      end
-
     end
 
-  end  
+    # e.g. :name_for_id(id)
+    define_method "#{self.offline_lookup_options[:field]}_for_#{self.offline_lookup_options[:key]}" do |key|
+      self.offline_lookup_values(key)
+    end
+
+    # e.g. :id_for_name(name)
+    define_method "#{self.offline_lookup_options[:key]}_for_#{self.offline_lookup_options[:field]}" do |value|
+      self.offline_lookup_values.find{|k, v| v.to_s == value.to_s}
+    end
+
+    # e.g. quick_lookup(:two_hour)
+    # not offline, but looks up by key which is usually indexed. Also syntactic sugar.
+    def quick_lookup(value)
+      key = self.offline_lookup_values.find{|k, v| v.to_s == value.to_s}
+      find_by(self.offline_lookup_options[:key] => key)
+    end
+    def quick_lookup!(value)
+      key = self.offline_lookup_values.find{|k, v| v.to_s == value.to_s}
+      find(key)
+    end
+  end
 end
 
-ActiveRecord::Base.class_eval { include OfflineIDLookup }
+ActiveRecord::Base.extend OfflineIDLookup::ActiveRecord
